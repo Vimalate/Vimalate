7d5530547a861a2d6c52895b579672faa1382b26
Vimalate <1902644369@qq.com>
2020-08-07 18:12:08 +0800

    接口

---
+++ test.js
@@ -0,0 +1,159 @@
+{
+    "code": 200,
+    "success": true,
+    "message": "成功.",
+    "data": {
+      "currentBusinessId": "",
+      "businessId": "",
+      "businessName": "",
+      "pageConfigData": [
+        {
+          "groupName": "组名1",
+          "surfaceConfigItem": [
+            {
+              "surfaceConfigId": "20bbb5cf17cb11e991a500ff010a3acf",
+              "controlName": "td",
+              "controlCode": "td",
+              "cellName": "检查项",
+              "defaultValue": "",
+              "isMandatory": "1",
+              "fieldName": "",
+              "className": "form_td_right",
+              "isEdit": "0",
+              "promptWord": "",
+              "groupName": ""
+            },
+            {
+              "surfaceConfigId": "20bbb5cf17cb11e991a500ff010a3acf",
+              "controlName": "文本框",
+              "controlCode": "input",
+              "cellName": "检查项",
+              "defaultValue": "",
+              "isMandatory": "1",
+              "fieldName": "item1",
+              "className": "form_td_noWidthRight",
+              "isEdit": "0",
+              "promptWord": "",
+              "groupName": ""
+            }
+          ]
+        },
+        {
+          "groupName": "组名2",
+          "surfaceConfigItem": [
+            {
+              "surfaceConfigId": "20bbb5cf17cb11e991a500ff010a3acf",
+              "controlName": "td",
+              "controlCode": "td",
+              "cellName": "检查项",
+              "defaultValue": "",
+              "isMandatory": "1",
+              "fieldName": "",
+              "className": "form_td_right",
+              "isEdit": "0",
+              "promptWord": "",
+              "groupName": ""
+            },
+            {
+              "surfaceConfigId": "20bbb5cf17cb11e991a500ff010a3acf",
+              "controlName": "文本框",
+              "controlCode": "input",
+              "cellName": "检查项",
+              "defaultValue": "",
+              "isMandatory": "1",
+              "fieldName": "item1",
+              "className": "form_td_noWidthRight",
+              "isEdit": "0",
+              "promptWord": "",
+              "groupName": ""
+            }
+          ]
+        }
+      ],
+      "superviseInfoData": [
+        {
+          "id": "40288432720165f201720168e8270000",
+          "projectId": "工程ID",
+          "projectCode": "工程编码",
+          "projectNum": "监督登记号",
+          "projectName": "工程名称",
+          "monitorPk": "专项检查主键(多个逗号隔开)",
+          "monitorName": "专项检查名称(多个逗号隔开)",
+          "status": 0,
+          "itemMonitorId": "be9d645cc59411eaa35f00ff7beea89a",
+          "itemMonitor": "管理员",
+          "item1": "检查类型",
+          "item2": "监督措施",
+          "dateItemRecord": "2020-08-04 16:53:36",
+          "radio1": 1,
+          "superviseInfoItemList": [
+            {
+              "bussinessId": "10ab2c6617cb11e991a500ff010a3acf",
+              "bussinessName": "专项检查名称1",
+              "superviseInfoThirdList": [
+                {
+                  "id": "1870274617cb11e991a500ff010a3acf",
+                  "superviseInfoId": "40288432720165f201720168e8270000",
+                  "superviseInfoItemId": "4028842273b759e70173b8ad0f270019",
+                  "bussinessId": "10ab2c6617cb11e991a500ff010a3acf",
+                  "bussinessName": "专项检查名称1",
+                  "groupName": "组名1",
+                  "item1": "检查项1",
+                  "item2": "检查内容1",
+                  "item3": "分值1",
+                  "item4": "检查情况1",
+                  "item5": "主要存在问题1"
+                },
+                {
+                  "id": "402881cb739f31e70173a43b6ed3003f",
+                  "superviseInfoId": "40288432720165f201720168e8270000",
+                  "superviseInfoItemId": "4028842273b759e70173b8ad0f270019",
+                  "bussinessId": "10ab2c6617cb11e991a500ff010a3acf",
+                  "bussinessName": "专项检查名称1",
+                  "groupName": "组名2",
+                  "item1": "检查项2",
+                  "item2": "检查内容2",
+                  "item3": "分值2",
+                  "item4": "检查情况2",
+                  "item5": "主要存在问题2"
+                }
+              ]
+            },
+            {
+              "bussinessId": "402881cb73ad6e8e0173af30936c010f",
+              "bussinessName": "专项检查名称2",
+              "superviseInfoThirdList": [
+                {
+                  "id": "1870274617cb11e991a500ff010a5ehj",
+                  "superviseInfoId": "40288432720165f201720168e8270000",
+                  "superviseInfoItemId": "4028842273b759e70173b8ad0f270020",
+                  "bussinessId": "402881cb73ad6e8e0173af30936c010f",
+                  "bussinessName": "专项检查名称2",
+                  "groupName": "组名",
+                  "item1": "检查项1",
+                  "item2": "检查内容1",
+                  "item3": "分值1",
+                  "item4": "检查情况1",
+                  "item5": "主要存在问题1"
+                },
+                {
+                  "id": "402881cb739f31e70173a43b6ed6113a",
+                  "superviseInfoId": "40288432720165f201720168e8270000",
+                  "superviseInfoItemId": "4028842273b759e70173b8ad0f270020",
+                  "bussinessId": "402881cb73ad6e8e0173af30936c010f",
+                  "bussinessName": "专项检查名称2",
+                  "groupName": "组名",
+                  "item1": "检查项2",
+                  "item2": "检查内容2",
+                  "item3": "分值2",
+                  "item4": "检查情况2",
+                  "item5": "主要存在问题2"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    "time": "2020-08-07 18:06:13"
+  }
